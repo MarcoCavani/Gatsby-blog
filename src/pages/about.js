67bfe5b6a284358f2503ee64dc712554c6cd76dd
MarcoCavani <cavaniaustralia@gmail.com>
2021-12-22 16:21:08 +0800

    routering

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Layout from '../components/layaout'
+const AboutPage = () =>{
+    return(
+        <Layout pageTitle="About Page">
+            <p>This is the about page!</p>
+        </Layout>
+    )
+}
+export default AboutPage
